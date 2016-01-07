b4064c1082a111e1a0e4241aaeba19fcc1eca4b3
Glen Peek <glen.peek@gmail.com>
2016-01-08 10:38:50 +1300

    update baseurl for jspm

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 System.config({
-  baseURL: "/",
+  baseURL: "/silverstripe-visual",
   defaultJSExtensions: true,
   transpiler: "babel",
   babelOptions: {
--- src/main.js
+++ src/main.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import CommentManagerComponent from './comment-manager-component';
+import VisualManagerComponent from './visual-manager-component';
 
-var wrapper = document.getElementById('comment-manager-component-wrapper');
+var wrapper = document.getElementById('visual-manager-component-wrapper');
 
 var props = {
-    source: 'data/comments.json',
-    endpoints: [
-      {
-        label: 'Approve',
-        name: 'approve',
-        type: 'success',
-        endpoint: wrapper.getAttribute('data-endpoint-approve')
-      },
-      {
-        label: 'Spam',
-        name: 'spam',
-        type: 'warning',
-        endpoint: wrapper.getAttribute('data-endpoint-spam')
-      },
-      {
-        label: 'Delete',
-        name: 'delete',
-        type: 'danger',
-        endpoint: wrapper.getAttribute('data-endpoint-delete')
-      }
-    ]
+    source: 'https://packagist.org/packages/'+wrapper.getAttribute('vendor')+'/'+wrapper.getAttribute('package')+'.json'
 };
 
 React.render(
-    <CommentManagerComponent {...props} />,
+    <VisualManagerComponent {...props} />,
     wrapper
 );
